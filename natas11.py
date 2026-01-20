f17c28b86a9fa1578b032c9cbccf3fd25fdc5241
davidoberst <juandavidbustosperez82@gmail.com>
2026-01-20 16:22:05 -0500

    XOR decrypt

--- natas11.py
+++ natas11.py
@@ -1,7 +1,16 @@
+#NATAS11 XOR KEY DECRYPT
+
 import base64
 
 #decode base64
 encodedCookie = "HmYkBwozJw4WNyAAFyB1VUcqOE1JZjUIBis7ABdmbU1GIjEJAyIxTRg="
 decodedC = base64.b64decode(encodedCookie)
 #encode XOR
+plaintext = b'{"showpassword":"no","bgcolor":"#ffffff"}' #b = bytes
+key = b'' #start constructing the key from bytes
+
+for i in range(len(decodedC)):
+    key += bytes([decodedC[i] ^ plaintext[i]])
+
+print(key)
 
